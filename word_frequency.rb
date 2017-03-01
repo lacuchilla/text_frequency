3da2725837ceff347f131a38c986f71487295553
Amy Hunter <amyrhunter@gmail.com>
2017-03-01 12:25:55 -0800

    Can now populate dictionary of words to search for using arguments from command line

--- word_frequency.rb
+++ word_frequency.rb
@@ -4,14 +4,20 @@ class Words
     @words_searched = {}
   end
   # filepath = ARGV[0]
-  def word_frequency(filepath = ARGV[0], words_to_search = ARGV[1])
+  def word_frequency(filepath = ARGV[0], words_to_search = ARGV[1..-1])
+    puts filepath
+    words_to_search.each do |word|
+      @words_searched[word] = 0
+    end
+
     file = File.open(filepath, "r")
     content = file.read
     content.each_line do |line|
       #save variable of each line, make it pass through regex to remove punctuation and newline characters
-      
+
       puts line
     end
+    puts @words_searched
     # puts content
   # def word_frequency(text_documents)#words to look for
     # IO.foreach(filepath) {|x| print "GOT", x}
